245fa60deed81866fc7f570f81dde5c619e7f050
CSL <3206725624@qq.com>
2022-06-28 09:15:15 +0800

    第一次修改

---
+++ src/main/java/com/rj/bd/Test.java
@@ -0,0 +1,16 @@
+package com.rj.bd;
+
+/**
+ * @desc
+ * @name CSL
+ * @date 2022-06-28 9:14
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("这是第一个GitHub项目");
+
+    }
+
+}
